dba894b0f3b79c343e42dec553634612f352b999
adjit <ajtawil@gmail.com>
2019-09-07 11:58:25 -0400

    Add project files.

---
+++ VTS-ToDo/Models/ToDoListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDo.Models
+{
+    public class ToDoListItem
+    {
+        public long Id { get; set; }
+        public string ToDoItem { get; set; }
+        public string Description { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
